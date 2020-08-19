0a0cd894147c5a95f5b9af46890f5517bc128b69
Saharak Manoo <saharakmanoo@outlook.com>
2020-08-19 09:34:42 +0700

    [Setup] React JS and Node

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import React, { PropTypes, Component } from 'react';
+import './style.css';
+
+class NotFound extends Component {
+	render() {
+		return (
+			<div>
+				<h1>404 : Not Found :(</h1>
+			</div>
+		);
+	}
+}
+
+export default NotFound;
